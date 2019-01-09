2ec781d06e72602478f4508a02c93abe1587e69a
Shannen Dolls <shannen_dolls@live.nl>
2019-01-09 11:59:05 +0100

    the user_availability page now also displays the last registered location and availability of a selected user

--- app/availability/routes.py
+++ app/availability/routes.py
@@ -1,5 +1,5 @@
 from flask_login import login_required
-from app.models import User
+from app.models import User, Checkin, Location
 from app.availability import bp
 from flask import render_template
 
@@ -16,7 +16,15 @@ def user_list():
 @login_required
 def user_availability(user_id):
     user = User.query.filter_by(id=user_id).first()
+    checkins = Checkin.query.filter_by(user_id=user_id)
+    checkin = checkins[-1]
+    location = Location.query.filter_by(id=checkin.location_id).first()
+    availability = checkin.availability
+    if availability == False:
+        availability="Not available"
+    else:
+        availability="Available"
     return render_template(
         'availability/user_availability.html', user=user,
-        title='Availability'
+        location=location, availability=availability, title='Availability'
     )
